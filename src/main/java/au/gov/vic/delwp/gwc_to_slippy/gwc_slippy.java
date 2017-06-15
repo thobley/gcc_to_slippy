d2ac023b67a07a0b063a4e36d7f697b8dafa4b6a
hobley <hobley@trents-mbp.gateway>
2017-06-15 22:17:05 +1000

    1st commit

--- src/main/java/au/gov/vic/delwp/gwc_to_slippy/gwc_slippy.java
+++ src/main/java/au/gov/vic/delwp/gwc_to_slippy/gwc_slippy.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package au.gov.vic.delwp.gwc_to_slippy;
 
 import java.io.File;
